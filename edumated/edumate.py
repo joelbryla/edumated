f27f7f4bad3fac69121e4fe5a8ddb96bd914a19d
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2019-01-24 09:33:08 +1100

    Remove pandas, move arument parsing elsewhere
    Tidy up date handling and username and password storing

--- edumated/edumate.py
+++ edumated/edumate.py
@@ -1,86 +1,38 @@
 #!/usr/bin/env python3
-import argparse
-import datetime
-import getpass
+from datetime import timedelta
+from getpass import getpass
 from os import path, mkdir
-import pandas as pd
 
 from .fetch import Fetcher
 from .googcal import CalendarTool
+from .util import daterange, parseargs
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--start_date",
-        type=datetime.date.fromisoformat,
-        default=datetime.datetime.now().date(),
-        help="Start date in ISO format, YYYY-MM-DD (Defaults to today)",
-    )
-    parser.add_argument(
-        "--end_date",
-        type=datetime.date.fromisoformat,
-        default=None,
-        help="End date in ISO format, YYYY-MM-DD",
-    )
+    args = parseargs()
 
-    parser.add_argument(
-        "--days", default=1, type=int, help="Days including start date to fetch"
-    )
-    parser.add_argument("--weeks", type=int, help="Weeks including start date to fetch")
-
-    parser.add_argument(
-        "--conf_folder",
-        default=path.join(path.expanduser("~"), "/.edumated/"),
-        type=str,
-        help="Config file containg calender id (default is ~/.edumated/",
-    )
-
-    args = parser.parse_args()
-
-    if args.weeks is not None:
-        args.days = args.weeks * 7
-
-    one_day_offset = datetime.timedelta(days=1)
-
-    if args.end_date is not None:
-        args.days = (args.end_date - args.start_date + one_day_offset).days
+    conf_file = path.join(args.conf_folder, "conf")
 
     if not path.isdir(args.conf_folder):
         mkdir(args.conf_folder)
 
-    pass_file = args.conf_folder + "pass"
-    cal_file = args.conf_folder + "cal"
-
-    if not path.isfile(pass_file):
+    if not path.isfile(conf_file):
         username = input("Username: ")
-        password = getpass.getpass(prompt="Password: ")
-        with open(pass_file, "w+") as file:
-            file.write(username + "\n")
-            file.write(password)
+        password = getpass(prompt="Password: ")
+        calendar_id = input("Google Calendar ID: ")
+        with open(conf_file, "w") as f:
+            f.write("\n".join([username, password, calendar_id]))
+    else:
+        with open(conf_file) as f:
+            username, password, calendar_id = map(lambda l: l.strip(), f.readlines())
 
-    with open(pass_file) as pass_f:
-        username = pass_f.readline().strip()
-        password = pass_f.readline().strip()
-
-    if not path.isfile(cal_file):
-        cal_id = input("Google Calendar ID: ")
-        with open(cal_file, "w+") as file:
-            file.write(cal_id)
-
-    with open(cal_file) as conf:
-        calendar_id = conf.readline().strip()
-
-    cal = CalendarTool(calendar_id, args.conf_folder)
+    calendar = CalendarTool(calendar_id, args.conf_folder)
     fetcher = Fetcher(username, password)
+    one_day_offset = timedelta(days=1)
+    dates = daterange(args.start_date - one_day_offset, args.end_date)
 
-    args.start_date = args.start_date - one_day_offset
-    dates = pd.date_range(args.start_date, periods=args.days)
-
-    print(
-        f"Fetching Events: {dates[0].date()+one_day_offset} to {dates[-1].date()+one_day_offset}"
-    )
-    edumate_day_data = fetcher.get_simple_dates(dates)
+    print(f"Fetching Events: {args.start_date} to {args.end_date}")
+    classes = fetcher.get_simple_dates(dates)
 
     print("Adding to Google Calendar")
-    cal.make_all_events(edumate_day_data)
+    calendar.make_all_events(classes)
