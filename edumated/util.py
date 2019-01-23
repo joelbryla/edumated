e3335c48840d930982f373379417336ad55507ba
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2019-01-24 09:32:03 +1100

    Move non logic functions out of main edumated file

---
+++ edumated/util.py
@@ -0,0 +1,46 @@
+import argparse
+import datetime
+from os import path, mkdir
+
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--conf-folder",
+        default=path.join(path.expanduser("~"), ".edumated"),
+        help="Config folder containg calender id, username, etc. (Default: ~/.edumated/",
+    )
+    parser.add_argument(
+        "--start-date",
+        default=datetime.datetime.now().date(),
+        help="Start date in ISO format, YYYY-MM-DD (Default: today)",
+    )
+    parser.add_argument(
+        "--end-date", type=str, help="End date in ISO format, YYYY-MM-DD"
+    )
+    parser.add_argument("--weeks", type=int, help="Weeks including start date to fetch")
+    parser.add_argument(
+        "--days", default=1, help="Days including start date to fetch (Default: 1)"
+    )
+    args = parser.parse_args()
+
+    if args.end_date:
+        args.end_date = datetime.datetime.strptime(args.end_date, "%Y-%m-%d")
+
+    if args.days:
+        args.end_date = args.start_date + datetime.timedelta(days=args.days)
+
+    if args.weeks:
+        args.end_date = args.start_date + datetime.timedelta(weeks=args.weeks)
+
+    if not path.isdir(args.conf_folder):
+        mkdir(args.conf_folder)
+
+    return args
+
+
+def daterange(start_date, end_date):
+    dates = []
+    for n in range(int((end_date - start_date).days)):
+        dates.append(start_date + datetime.timedelta(n))
+    return dates
